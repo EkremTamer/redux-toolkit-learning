cd03fd5af71a701ddfe07031f8f9737a2098755d
EkremTamer <ekotamer@gmail.com>
2023-06-25 14:50:23 -0500

    toolkit learning

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import UserShow from './components/user_show'
+import { setFirstname, setAge, setLastname } from "./redux/userSlice";
 
 function App() {
+  //const authState = useSelector(state=> state.auth)
+  const userState = useSelector(state=> state.user)
+  const dispatch = useDispatch() 
+
+  console.log('>>App AuthState', userState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-2">
+      <div className="row">
+        <div className="col-6">
+          <div className="row" >
+            <div className="col-12">
+              <label className="form-label">First Name</label>
+              <input 
+              type="text" 
+              className="form-control" 
+              placeholder="Enter your first name"
+              onChange={event => dispatch(setFirstname(event.target.value))}/>
+            </div>
+            <div className="col-12 mt-3">
+              <label className="form-label">Last Name</label>
+              <input 
+              type="text" 
+              className="form-control" 
+              placeholder="Enter your last name"
+              onChange={event => dispatch(setLastname(event.target.value))}/>
+            </div>
+            <div className="col-12 mt-3">
+              <label className="form-label">Age</label>
+              <input 
+              type="number" 
+              className="form-control" 
+              placeholder="Enter your age"
+              onChange={event => dispatch(setAge(event.target.value))}/>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <UserShow/>
+        </div>
+      </div>
     </div>
   );
 }
